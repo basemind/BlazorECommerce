3b55a9043eb91b12f8b0555abe24a53f79bc9ef5
basemind <fatihsamancioglu@gmail.com>
2022-03-28 17:34:39 +0300

    fisrt

---
+++ BlazorECommerce/BlazorECommerce/Shared/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorECommerce.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+
+        [Column(TypeName= "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+    }
+}
